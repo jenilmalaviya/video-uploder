982194b2a52d31fb877f3ea383dfa19d085aa67d
jenil <jenilmalaviya25@gmail.com>
2024-05-07 16:05:54 +0530

    add new routes for user controller

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,6 +4,13 @@ import {
   logoutUser,
   registerUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWhatchHistory,
 } from "../controllerss/user.controllerss.js";
 import { upload } from "../middleweres/multer.js";
 import { verifJWT } from "../middleweres/auth.middlerwares.js";
@@ -27,5 +34,16 @@ router.route("/login").post(loginUser);
 // sequired router
 router.route("/logout").post(verifJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifJWT, changeCurrentPassword);
+router.route("/current-user").get(verifJWT, getCurrentUser);
+router.route("/update-account").patch(updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifJWT, upload.single("/coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifJWT, getUserChannelProfile);
+router.route("WatchHistory").get(verifJWT, getWhatchHistory);
 
 export default router;
