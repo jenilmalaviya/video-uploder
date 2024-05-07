450579d8381958280227ab517b1ff234f0f419f2
jenil <jenilmalaviya25@gmail.com>
2024-05-07 11:28:27 +0530

    "Add coverImage, email to getUserChannelProfile response and throw error if channel does not exist"
    This commit adds the 'coverImage' and 'email' fields to the response of the 'getUserChannelProfile' controller function.

--- src/controllerss/user.controllerss.js
+++ src/controllerss/user.controllerss.js
@@ -1,4 +1,4 @@
-import { application } from "express";
+import { application, response } from "express";
 import { User } from "../models/User.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -357,10 +357,20 @@ const getUserChannelProfile = asynchandler(async (req, res) => {
         subscriberCount: 1,
         chanalSubscribedToCount: 1,
         isSubscribed: 1,
-        avatar,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
       },
     },
   ]);
+
+  if (!channel?.length) {
+    throw new ApiError(404, "cheanal dose not exesist");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, channel[0], "user chenal fached succesfully"));
 });
 
 export {
