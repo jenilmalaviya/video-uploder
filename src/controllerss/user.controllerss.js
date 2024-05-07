5e43f84b41a14b247c5adeeda8d7ca0f6f92c370
jenil <jenilmalaviya25@gmail.com>
2024-05-07 15:29:08 +0530

    Add getWatchHistory endpoint

--- src/controllerss/user.controllerss.js
+++ src/controllerss/user.controllerss.js
@@ -5,6 +5,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asynchandler } from "../utils/asyncHendler.js";
 import { uploadcloudinary } from "../utils/cloudnary.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const generateAccessAndRefereshToken = async (userId) => {
   try {
@@ -373,6 +374,59 @@ const getUserChannelProfile = asynchandler(async (req, res) => {
     .json(new ApiResponse(200, channel[0], "user chenal fached succesfully"));
 });
 
+const getWhatchHistory = asynchandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "Video",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "wathcHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    userName: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "watch history fached successFully"
+      )
+    );
+});
+
 export {
   registerUser,
   loginUser,
@@ -383,4 +437,5 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
+  getWhatchHistory,
 };
